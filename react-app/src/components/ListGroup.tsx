a20afed81ffb91e95539f9adc191b12707e83368
Shrimp556 <66566803+ThosL@users.noreply.github.com>
2024-06-26 03:04:36 -0500

    Added simple example of state hook usage

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,5 +1,8 @@
+import { useState } from "react";
+
+
 function ListGroup() {
-    const items= [
+    let items = [
         'New York',
         'Kansas City',
         'Oklahoma City',
@@ -7,21 +10,30 @@ function ListGroup() {
         'St. Louis',
         'Niotaze'
     ];
-
+    /*this is a hook to allow us to tap into built-in function in react */
+    const [selectedIndex,setSelectedIndex] = useState(-1);
+    /*arr[0] //selected index variable
+    arr[1] //updater function
+*/
     const getMessage = () => {
 
-        return items.length === 0 ? <p>No items found.</p>: <p>{items.length} items found.</p>;
+        return items.length === 0 ? <p>No items found.</p> : <p>{items.length} items found.</p>;
     }
-    return(
-    <>
-    {getMessage()}
-    <h1>List</h1>
-        <ul className="list-group"> 
-            {items.map(item => 
-                <li key={item} className="list-group-item">{item}</li>)
-            }
-        </ul>
-    </>
+    return (
+        <>
+            {getMessage()}
+            <h1>List</h1>
+            <ul className="list-group ">
+                {items.map((item, index) =>
+                    <li 
+                        key={item} 
+                        onClick={ ()  => {setSelectedIndex(index);}} 
+                        className={ selectedIndex === index ? "list-group-item active": "list-group-item" }>
+                            {item}
+                    </li>)
+                }
+            </ul>
+        </>
     );
 }
 
